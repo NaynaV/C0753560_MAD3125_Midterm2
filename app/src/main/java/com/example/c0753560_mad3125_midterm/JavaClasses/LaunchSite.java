7713e79e66d1bd5dfc42e41940983dd86bce90f0
NaynaV <nainavaghasiya9@gmail.com>
2019-08-09 00:33:30 -0400

    [Nayna] Launch Site java classes added with getter setter

--- app/src/main/java/com/example/c0753560_mad3125_midterm/JavaClasses/LaunchSite.java
+++ app/src/main/java/com/example/c0753560_mad3125_midterm/JavaClasses/LaunchSite.java
@@ -1,4 +1,46 @@
 package com.example.c0753560_mad3125_midterm.JavaClasses;
 
 public class LaunchSite {
+    private String siteID;
+    private String siteName;
+    private String siteNameLong;
+
+    public LaunchSite(String siteID, String siteName, String siteNameLong) {
+        this.siteID = siteID;
+        this.siteName = siteName;
+        this.siteNameLong = siteNameLong;
+    }
+
+    public String getSiteID() {
+        return siteID;
+    }
+
+    public void setSiteID(String siteID) {
+        this.siteID = siteID;
+    }
+
+    public String getSiteName() {
+        return siteName;
+    }
+
+    public void setSiteName(String siteName) {
+        this.siteName = siteName;
+    }
+
+    public String getSiteNameLong() {
+        return siteNameLong;
+    }
+
+    public void setSiteNameLong(String siteNameLong) {
+        this.siteNameLong = siteNameLong;
+    }
+
+    @Override
+    public String toString() {
+        return "LaunchSite{" +
+                "siteID='" + siteID + '\'' +
+                ", siteName='" + siteName + '\'' +
+                ", siteNameLong='" + siteNameLong + '\'' +
+                '}';
+    }
 }
