932f15c80a1054286167908eea5c9c2718ea9dfc
NaynaV <nainavaghasiya9@gmail.com>
2019-08-09 00:35:22 -0400

    [Nayna] FlightMain java classes added with constructor

--- app/src/main/java/com/example/c0753560_mad3125_midterm/JavaClasses/FlightMain.java
+++ app/src/main/java/com/example/c0753560_mad3125_midterm/JavaClasses/FlightMain.java
@@ -1,4 +1,114 @@
 package com.example.c0753560_mad3125_midterm.JavaClasses;
 
-public class FlightMain {
+public class FlightMain
+{
+    private String filghtNumber;
+    private String missionName;
+    private String upcoming;
+    private String launchYear;
+    private String launchWindow;
+    private String details;
+    Rocket rocket;
+    LaunchSite launchSite;
+    Links links;
+
+    public FlightMain(String filghtNumber, String missionName, String upcoming, String launchYear, String launchWindow, String details, Rocket rocket, LaunchSite launchSite, Links links)
+    {
+        this.filghtNumber = filghtNumber;
+        this.missionName = missionName;
+        this.upcoming = upcoming;
+        this.launchYear = launchYear;
+        this.launchWindow = launchWindow;
+        this.details = details;
+        this.rocket = rocket;
+        this.launchSite = launchSite;
+        this.links = links;
+    }
+
+    public String getFilghtNumber() {
+        return filghtNumber;
+    }
+
+    public void setFilghtNumber(String filghtNumber) {
+        this.filghtNumber = filghtNumber;
+    }
+
+    public String getMissionName() {
+        return missionName;
+    }
+
+    public void setMissionName(String missionName) {
+        this.missionName = missionName;
+    }
+
+    public String getUpcoming() {
+        return upcoming;
+    }
+
+    public void setUpcoming(String upcoming) {
+        this.upcoming = upcoming;
+    }
+
+    public String getLaunchYear() {
+        return launchYear;
+    }
+
+    public void setLaunchYear(String launchYear) {
+        this.launchYear = launchYear;
+    }
+
+    public String getLaunchWindow() {
+        return launchWindow;
+    }
+
+    public void setLaunchWindow(String launchWindow) {
+        this.launchWindow = launchWindow;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public Rocket getRocket() {
+        return rocket;
+    }
+
+    public void setRocket(Rocket rocket) {
+        this.rocket = rocket;
+    }
+
+    public LaunchSite getLaunchSite() {
+        return launchSite;
+    }
+
+    public void setLaunchSite(LaunchSite launchSite) {
+        this.launchSite = launchSite;
+    }
+
+    public Links getLinks() {
+        return links;
+    }
+
+    public void setLinks(Links links) {
+        this.links = links;
+    }
+
+    @Override
+    public String toString() {
+        return "FlightMain{" +
+                "filghtNumber='" + filghtNumber + '\'' +
+                ", missionName='" + missionName + '\'' +
+                ", upcoming='" + upcoming + '\'' +
+                ", launchYear='" + launchYear + '\'' +
+                ", launchWindow='" + launchWindow + '\'' +
+                ", details='" + details + '\'' +
+                ", rocket=" + rocket +
+                ", launchSite=" + launchSite +
+                ", links=" + links +
+                '}';
+    }
 }
